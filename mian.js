9fd83f92d6cd072048953cc949d0f5dedeb6c881
Ishigamitempest0 <134192518+Jerald509@users.noreply.github.com>
2025-08-23 04:37:49 -0700

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,33 @@
+let progress = true; 
+console.log(typeof progress);
+
+const student ={
+gender:" female",
+status:"single",s
+age:"29"
+}
+console.log(student.status);//single
+console.log(student.gender);//female
+console.log(student.age);//age
+console.log(typeof student);//Object
+
+const person={
+  name:"sim john",
+  gender:"female",
+  age:"30",
+  heircolor:"green",
+  height:"5'1"
+}
+ 
+ var myAge="20";//
+ var yourAge=20;//
+ var grade=null;//
+ console.log(typeof myAge);//string
+ console.log(typeof yourAge);//number
+ console.log(typeof grade);//object
+ 
+console.log(person.name);
+console.log(person.age);
+console.log(person.gender);
+console.log(person.weight);
+console.log(person.heircolor);
